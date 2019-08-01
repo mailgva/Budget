191fa46df8b35ab74fa4d0a88412e2578f3c534a
Vladimir Gorbatenko <mail.gva@gmail.com>
2019-08-01 21:35:27 +0300

    Update hr.bat

--- hr.bat
+++ hr.bat
@@ -1,3 +1,3 @@
-mvn spring-boot:run
-rem call mvn -B -s settings.xml -DskipTests=true clean package
-rem call java -jar target/dependency/webapp-runner.jar target/*.war
+rem mvn spring-boot:run
+call mvn -B -s settings.xml -DskipTests=true clean package
+call java -jar target/dependency/webapp-runner.jar target/*.war
