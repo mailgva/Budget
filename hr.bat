9fb18d60e3f9c8d2217f544d37a78dc10932a9a3
Vladimir Gorbatenko <mail.gva@gmail.com>
2020-04-24 00:28:32 +0300

    created link for last activity which opens statistic page on date

--- hr.bat
+++ hr.bat
@@ -1,8 +1,3 @@
 rem mvn spring-boot:run
-if [ -z ${GDRIVE_CLIENT_ID} ]; then export GDRIVE_CLIENT_ID=''; fi
-if [ -z ${GDRIVE_SECRET} ]; then export GDRIVE_SECRET=''; fi
-if [ -z ${GDRIVE_REFRESH_TOKEN} ]; then export GDRIVE_REFRESH_TOKEN=''; fi
-if [ -z ${GDRIVE_FOLDERID} ]; then export GDRIVE_FOLDERID=''; fi
-
 call mvn -B -s settings.xml -DskipTests=true clean package
 call java -jar target/dependency/webapp-runner.jar target/*.war
