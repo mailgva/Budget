c5e9a6a1079f641017beb191f58a5b0491808327
Vladimir Gorbatenko <mail.gva@gmail.com>
2021-01-27 21:59:44 +0200

    added icons for mobile home screen and change readme file

---
+++ src/main/java/com/gorbatenko/budget/web/TestRestController.java
@@ -0,0 +1,30 @@
+package com.gorbatenko.budget.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("rest")
+public class TestRestController {
+    /*
+    @Autowired
+    private RequestMappingHandlerMapping requestHandlerMapping;
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @GetMapping(value = "/endpoints")
+    public List<String> endpoints() {
+        return requestHandlerMapping.getHandlerMethods()
+                .entrySet()
+                .stream()
+                .map(entry -> entry.getKey().toString() + ": " + entry.getValue().getBeanType().getName() + "#" + entry.getValue().getMethod().getName())
+                .collect(Collectors.toList());
+    }
+    */
+}
