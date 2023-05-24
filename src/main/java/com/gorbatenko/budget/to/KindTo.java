7aadf3cf0d05815bb177d3ba38930e10472455cd
Горбатенко Владимир Александрович <vhorbate@cisco.com>
2023-05-24 12:01:48 +0300

    small fixes

--- src/main/java/com/gorbatenko/budget/to/KindTo.java
+++ src/main/java/com/gorbatenko/budget/to/KindTo.java
@@ -1,12 +1,17 @@
 package com.gorbatenko.budget.to;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.gorbatenko.budget.BaseEntity;
 import com.gorbatenko.budget.model.Type;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import jakarta.validation.constraints.NotNull;
+import java.io.IOException;
 
 @Data
 @NoArgsConstructor
@@ -18,11 +23,19 @@ public class KindTo extends BaseEntity {
     @NotNull
     private Type type;
 
+    @JsonDeserialize(using = OnOffDeserializer.class)
     private boolean hidden = false;
 
     public KindTo(Type type, String name, boolean hidden) {
         this.type = type;
         this.name = name;
         this.hidden = hidden;
+    }
+
+    public static class OnOffDeserializer extends JsonDeserializer<Boolean> {
+        @Override
+        public Boolean deserialize(JsonParser parser, DeserializationContext context) throws IOException {
+            return "on".equalsIgnoreCase(parser.getText());
+        }
     }
 }
